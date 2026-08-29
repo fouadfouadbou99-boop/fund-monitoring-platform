655958f79b749d1117587e0ae6d58bdb8461a018
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-29 09:57:53 +0100

    Add function to extract text from DOCX files

---
+++ extraction/docx_reader.py
@@ -0,0 +1,14 @@
+from docx import Document
+
+
+def extract_text_from_docx(uploaded_file):
+
+    document = Document(uploaded_file)
+
+    text = ""
+
+    for paragraph in document.paragraphs:
+
+        text += paragraph.text + "\n"
+
+    return text
