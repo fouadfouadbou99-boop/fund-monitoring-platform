1718d88059a5e156f82ab3269b5a324b22bb247e
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-29 15:24:17 +0100

    Refactor extractor.py by removing unused code
    Removed unused imports and function for document analysis.

--- ai/extractor.py
+++ ai/extractor.py
@@ -1,21 +1,7 @@
-import streamlit as st
-import google.generativeai as genai
+prompt = f"""
+{PROMPT}
 
-from ai.prompts import PROMPT
+DOCUMENT :
 
-
-def analyze_document(document_text):
-
-    genai.configure(
-        api_key=st.secrets["GEMINI_API_KEY"]
-    )
-
-    model = genai.GenerativeModel(
-        "models/gemini-3.6-flash"
-    )
-
-    response = model.generate_content(
-        "Réponds uniquement : OK"
-    )
-
-    return response.text
+{document_text[:5000]}
+"""
