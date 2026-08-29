826662bfbcd089377bd1188bf7fb499791eb497f
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-29 10:17:55 +0100

    Implement initial RulesEngine class structure

---
+++ monitoring/rules_engine.py
@@ -0,0 +1,13 @@
+class RulesEngine:
+
+    def __init__(self):
+
+        self.rules = {}
+
+    def load_rules(self, rules):
+
+        self.rules = rules
+
+    def get_rule(self, key):
+
+        
